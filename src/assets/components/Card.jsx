0918a1cb399aab20a7be72067b6f419a1fa543fc
PatrizioFiori <patrizio.fiori1995@gmail.com>
2025-01-29 16:50:39 +0100

    Add: bandiera in base alla lingua gestita (da sistemare i casi non gestiti)

--- src/assets/components/Card.jsx
+++ src/assets/components/Card.jsx
@@ -1,6 +1,10 @@
 import { useMainContext } from "../contexts/MainContext"
 
 const Card = ({ item }) => {
+
+    const { bandierina } = useMainContext()
+    const bandiera = bandierina(item.original_language)
+
     return (
 
         <div className="card card-custom">
@@ -13,7 +17,8 @@ const Card = ({ item }) => {
             <div className="card-custom-info text-center">
                 <h5>{item.title}</h5>
                 <h6 className="text-dark-emphasis">{item.original_title}</h6>
-                <p>{item.original_language}</p>
+                <img className="w-25" src={bandiera} alt={item.original_language} />
+                <p>{bandiera}</p>
                 <p>{item.vote_average}</p>
                 <p>{item.overview}</p>
             </div>
